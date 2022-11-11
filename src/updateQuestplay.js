30bba134feb3431c7f0dfb3df80596f10fa70bd5
Bryan Wee <bryanwee023@gmail.com>
2022-11-11 16:49:10 +0000

    Add version check and commit

--- src/updateQuestplay.js
+++ src/updateQuestplay.js
@@ -3,14 +3,42 @@ import chalk from 'chalk';
 import dotenv from 'dotenv';
 import { navigateToMainDirectory } from './utils/navigation.js';
 import { QuestDownloader } from './utils/downloader.js';
+import { simpleGit } from 'simple-git';
 
 import { 
   CREDENTIALS_NOT_FOUND_MESSAGE, 
+  UNCOMMITTED_FILES_BEFORE_UPDATE_MESSAGE
 } from './utils/messages.js';
+import { isLatestVersion, remoteVersion } from './utils/versions.js';
+
+const git = simpleGit();
 
 export async function updateQuestplay() {
 
   navigateToMainDirectory();
+  console.log();
+
+  // (1) Ensure all changes saved
+  const statusSummary = await git.status()
+  if (statusSummary.files.length) {
+    console.log(UNCOMMITTED_FILES_BEFORE_UPDATE_MESSAGE);
+    process.exit(1);
+  }
+
+  // (2) Look for update
+  if (isLatestVersion()) {
+    console.log(chalk.yellow("Questplay is up-to-date.\n"));
+    process.exit(0);
+  }
+
+  // (3) Pull update
+  await pullUpdate();
+
+  console.log();
+
+}
+
+async function pullUpdate() {
 
   console.log(chalk.green("\nUpdating Questplay..."));
 
@@ -30,6 +58,7 @@ export async function updateQuestplay() {
   console.log(chalk.green("\nInstalling Questplay..."));
   await authDownloader.installSubpackage();
 
-  console.log();
+  await git.commit(`Update Questplay to ${remoteVersion()}`);
+  console.log(chalk.green("Update committed\n"));
 
 }
