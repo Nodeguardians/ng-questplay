3fd13a1d69a20cf0a60f6b0cb255763cc3a06ede
MathisTestnet <120141642+MathisTestnet@users.noreply.github.com>
2022-12-09 00:21:10 +0000

    #build-tutorial

---
+++ campaigns/tutorial/build-tutorial/contracts/HelloGuardian.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract HelloGuardian {
+
+    /// @return "Hello Guardian"
+    function hello() external pure returns (string memory) {
+        return "Hello Guardian";
+    }
+
+}
---
+++ campaigns/tutorial/build-tutorial/hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.16",
+};
