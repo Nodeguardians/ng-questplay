53bee49902fbcf1650cc4cb023c31567cd83eb76
Bryan Wee <bryanwee023@gmail.com>
2023-09-10 01:26:48 +0800

    Improve help messages

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 import { findQuest } from "./src/findQuest.js";
 import {
-  FIND_HELP_MESSAGE,
-  MAIN_HELP_MESSAGE,
-  TEST_HELP_MESSAGE,
-  SUBMIT_HELP_MESSAGE,
-  UPDATE_HELP_MESSAGE,
-  BRIDGE_HELP_MESSAGE,
-  SET_FRAMEWORK_HELP_MESSAGE,
   TITLE,
   WRONG_DIRECTORY_MESSAGE,
 } from "./src/utils/messages.js";
@@ -22,53 +15,58 @@ import { Command } from "commander";
 
 const program = new Command();
 
+function myParseInt(value, dummyPrevious) {
+    // parseInt takes a string and a radix
+    const parsedValue = parseInt(value, 10);
+    if (isNaN(parsedValue)) {
+      throw new commander.InvalidArgumentError('Not a number.');
+    }
+    return parsedValue;
+  }
+
 if (!process.cwd().startsWith(mainPath())) {
   console.log(TITLE);
   console.log(WRONG_DIRECTORY_MESSAGE);
   process.exit(1);
 }
 
-function myParseInt(value, dummyPrevious) {
-  // parseInt takes a string and a radix
-  const parsedValue = parseInt(value, 10);
-  if (isNaN(parsedValue)) {
-    throw new commander.InvalidArgumentError('Not a number.');
-  }
-  return parsedValue;
-}
-
 program
-  .name("Node Guardiands quest CLI")
+  .name("quest")
   .description(
-    "CLI to download and submit quests to the Node Guardians servers"
-  )
-  .version("0.0.1");
+    "CLI application to manage Node Guardians quests."
+  );
 
-// program.addHelpText(MAIN_HELP_MESSAGE);
+program.configureOutput({
+    writeOut: (str) => {
+        // For help messages:
+        // commander.js adds [options] in usage example, just for `--help`
+        // This might be confusing for users.
+        if (str.startsWith("Usage:")) {
+            str = str.replace(" [options]", "");
+        };
+        process.stdout.write(chalk.grey(`\n${str}\n`));
+    },
+    writeErr: (str) => process.stdout.write(chalk.red(`\n${str}\n`))
+});
 
 program
   .command("test")
-  .description("Run local tests for the current quest")
-  .argument("[part]", "Part number of the quest to test", myParseInt)
+  .description("Run a quest's local test(s). If the index is unspecified, test all parts.\n" +
+  "Must be ran in a valid quest directory.")
+  .argument("[index]", "Part of quest to test (e.g. 1, 2, 3)", myParseInt)
   .allowExcessArguments(false)
-  .exitOverride((err) => {
-    console.log(TEST_HELP_MESSAGE);
-  })
   .action(async (part) => {
     await runTests(part);
   });
 
 program
   .command("set-framework")
-  .description("Set the framework for the current quest")
-  .argument("<framework>", "Framework to set : foundry or hardhat")
+  .description("Set the default testing framework for local tests.")
+  .argument("<framework>", "Framework of choice [hardhat | foundry]")
   .allowExcessArguments(false)
-  .exitOverride((err) => {
-    console.log(SET_FRAMEWORK_HELP_MESSAGE);
-  })
   .action(async (framework) => {
     if (framework != "foundry" && framework != "hardhat") {
-      console.log(chalk.red("\nERROR: Unrecognized framework"));
+      console.log(chalk.red("\nerror: Unrecognized framework"));
       return;
     }
 
@@ -77,12 +75,9 @@ program
 
 program
   .command("find")
-  .description("Find an download a quest")
-  .argument("[slug]", "Slug of the quest to find")
+  .description("Find a quest in the repository. Queries for a name if name is unspecified.")
+  .argument("[name]", "Name of quest to find.")
   .allowExcessArguments(false)
-  .exitOverride((err) => {
-    console.log(FIND_HELP_MESSAGE);
-  })
   .action(async (slug) => {
     if (slug === undefined) {
       const answers = await inquirer.prompt({
@@ -98,39 +93,42 @@ program
 
 program
   .command("bridge")
-  .description("Bridge a quest to the Node Guardians servers")
-  .argument("<hash>", "32 bytes hash")
+  .description("Query bridge signer for a signature.")
+  .argument("<bridge-hash>", "32-byte bridge hash obtained from quest's contract")
   .allowExcessArguments(false)
-  .exitOverride((err) => {
-    console.log(BRIDGE_HELP_MESSAGE);
-  })
   .action(async (hash) => {
     await bridge(hash);
   });
 
 program
   .command("update")
-  .description("Update the questplay repository")
-  .option("--new-remote <remote>", "Set a new remote target for the questplay update")
+  .description("Update Questplay to the latest version.")
+  .option("--new-remote <remote>", "Set a new remote target")
   .allowExcessArguments(false)
-  .exitOverride((err) => {
-    console.log(UPDATE_HELP_MESSAGE);
-  })
   .action(async (options) => {
     await updateQuestplay(options.newRemote);
   });
 
 program
   .command("submit")
-  .description("Submit a build quest solution to the Node Guardians servers")
-  .option("--set-upstream", "set the upstream branch alongside the submission")
-  .option("--listen", "wait for the server response")
+  .description("Submit a quest to nodeguardians.io for verification.")
+  .option("--set-upstream", "Push a new branch upstream")
+  .option("--listen", "Listen for server response")
   .allowExcessArguments(false)
-  .exitOverride((err) => {
-    console.log(SUBMIT_HELP_MESSAGE);
-  })
   .action(async (options) => {
     await submitQuest(options.setUpstream, options.listen);
   });
+
+if (process.argv.length == 2) {
+    console.log(TITLE);
+    // Add colour
+    let helpInformation = chalk.grey(program.helpInformation()
+        .replace(/Options(.|\n|)*$/, (match, _) => chalk.cyan(match)));
+    // Remove [options] from usage example
+    helpInformation = helpInformation.replace(" [options]", "");
+    console.log(helpInformation);
+
+    process.exit(0);
+}
 
 program.parseAsync();
